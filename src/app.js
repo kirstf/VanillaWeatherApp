f0e3fae7cae497a21beb6b8da2d2592ca497db1f
Kirsten Fenwick <kfretouch@gmail.com>
2021-12-02 13:09:58 +0000

    Formatted date and icon from api call

--- src/app.js
+++ src/app.js
@@ -20,26 +20,42 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       ` 
               <div class="col-2">
-                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div>
                 <img
-                  src="https://openweathermap.org/img/wn/04d@2x.png"
+                  src="https://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
                   alt=""
                   id="icon"
                   width="42px"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">12°</span> |
-                  <span class="weather-forecast-temperature-min">8°</span>
+                  <span class="weather-forecast-temperature-max">${
+                    forecastDay.temp.max
+                  }</span> |
+                  <span class="weather-forecast-temperature-min">${
+                    forecastDay.temp.min
+                  }</span>
                 </div>
               </div>
             
