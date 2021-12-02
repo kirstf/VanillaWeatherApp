cd866d71c4c95c7c6fe36d171ef4dc3cdaee6673
Kirsten Fenwick <kfretouch@gmail.com>
2021-12-02 13:25:45 +0000

    Fixed forecast to show correct amount of days and rounded the temperature

--- src/app.js
+++ src/app.js
@@ -33,10 +33,11 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      ` 
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        ` 
               <div class="col-2">
                 <div class="weather-forecast-date">${formatDay(
                   forecastDay.dt
@@ -50,16 +51,17 @@ function displayForecast(response) {
                   width="42px"
                 />
                 <div class="weather-forecast-temperature">
-                  <span class="weather-forecast-temperature-max">${
+                  <span class="weather-forecast-temperature-max">${Math.round(
                     forecastDay.temp.max
-                  }</span> |
-                  <span class="weather-forecast-temperature-min">${
+                  )}°</span> |
+                  <span class="weather-forecast-temperature-min">${Math.round(
                     forecastDay.temp.min
-                  }</span>
+                  )}°</span>
                 </div>
               </div>
             
   `;
+    }
   });
   forecastElement.innerHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
